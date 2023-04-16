e45af5892f111f6f324a273d3950ef4f27722477
kimmyfek <kimmyfek@gmail.com>
2023-04-16 11:22:48 -0600

    prettify graph page

--- src/Graphs/primary-role-chart.js
+++ src/Graphs/primary-role-chart.js
@@ -1,42 +1,48 @@
-import React from 'react';
-import Chart from 'chart.js/auto';
+import React from "react";
+import Chart from "chart.js/auto";
 
 function PrimaryRoleChart() {
   React.useEffect(() => {
-    let costTotal = document.querySelector('.costTotal');
-    let respTotal = document.querySelector('.respTotal');
-    let hrsTotal = document.querySelector('.hrsTotal');
-    var ctx = document.getElementById('myChart2').getContext('2d');
+    let costTotal = document.querySelector(".costTotal");
+    let respTotal = document.querySelector(".respTotal");
+    let hrsTotal = document.querySelector(".hrsTotal");
+    var ctx = document.getElementById("myChart2").getContext("2d");
     var myChart2 = new Chart(ctx, {
-      type: 'bar',
+      type: "bar",
       data: {
-        labels: ['Investigation Team Management', 'Communicable Disease Investigator', 'Communications', 'Enviromental Health', 'Epidemiology', 'Laboratory', 'Student Interviewer'],
-        datasets: [{
-          label: 'Hours worked during Outbreak',
-          data: [3847.65, 208, 269.50, 772.67, 14398.29, 2414.68, 171],
-          backgroundColor: [
-						'rgb(0,209,178)',
-						'rgb(77,104,227)',
-						'rgb(13,67,149)',
-						'rgb(81,190,237)',
-						'rgb(174,228,251)',
-						'rgb(0,148,126)',
-						'rgb(0,148,126)'
-          ],
-          borderColor: [
-            'rgba(255, 99, 132, 1)'
-          ],
-          borderWidth: 1
-        },
-        ]
+        labels: [
+          "Investigation Team Management",
+          "Communicable Disease Investigator",
+          "Communications",
+          "Enviromental Health",
+          "Epidemiology",
+          "Laboratory",
+          "Student Interviewer",
+        ],
+        datasets: [
+          {
+            label: "Hours worked during Outbreak",
+            data: [3847.65, 208, 269.5, 772.67, 14398.29, 2414.68, 171],
+            backgroundColor: [
+              "rgb(112,128,14)",
+              "rgb(255,255,0)",
+              "rgb(255,165,0)",
+              "rgb(255,255,0)",
+              "rgb(0,255,0)",
+              "rgb(70,130,180)",
+            ],
+            borderColor: ["rgba(255, 99, 132, 1)"],
+            borderWidth: 1,
+          },
+        ],
       },
       options: {
         scales: {
           y: {
-            beginAtZero: true
-          }
-        }
-      }
+            beginAtZero: true,
+          },
+        },
+      },
     });
   }, []);
 
@@ -50,4 +56,4 @@ function PrimaryRoleChart() {
   );
 }
 
-export default PrimaryRoleChart
+export default PrimaryRoleChart;
