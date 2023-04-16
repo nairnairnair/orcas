29c3c608570e8731088e6e50d2f1ecad319c0cc2
kimmyfek <kimmyfek@gmail.com>
2023-04-16 12:31:32 -0600

    more styling

--- src/Form/FormCosts.js
+++ src/Form/FormCosts.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { Link } from "react-router-dom"
-import MyReports from "../MyReports/MyReports"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import MyReports from "../MyReports/MyReports";
 
 const FormCosts = () => {
   const [formData, setFormData] = useState({
-    travel: '',
-    courier: '',
-    hours: ''
+    travel: "",
+    courier: "",
+    hours: "",
   });
 
   const handleChange = (event) => {
@@ -23,45 +23,50 @@ const FormCosts = () => {
   };
 
   return (
-  <section>
-    <Link to="/">
-      <button>Log Out</button>
-    </Link>
-    <form onSubmit={handleSubmit}>
-      <label>
-        Travel Costs?
-        <input
-          type="text"
-          name="travel"
-          value={formData.travel}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Courier Costs?
-        <input
-          type="text"
-          name="courier"
-          value={formData.courier}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-      <label>
-        Hours Worked?
-        <input
-          type="text"
-          name="hours"
-          value={formData.hours}
-          onChange={handleChange}
-        />
-      </label>
-      <Link to="/graphs">
-        <button type="submit">Submit and view Graphs</button>
-      </Link>
-    </form>
-    <MyReports/>
+    <section>
+      <form class="form-costs" onSubmit={handleSubmit}>
+        <label className="label">
+          Travel Costs?
+          <input
+            className="input"
+            type="text"
+            name="travel"
+            value={formData.travel}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label className="label">
+          Courier Costs?
+          <input
+            className="input"
+            type="text"
+            name="courier"
+            value={formData.courier}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label className="label">
+          Hours Worked?
+          <input
+            className="input"
+            type="text"
+            name="hours"
+            value={formData.hours}
+            onChange={handleChange}
+          />
+        </label>
+        <Link to="/graphs">
+          <button
+            class="is-fullwidth has-background-black has-text-white button"
+            type="submit"
+          >
+            Submit and View Graphs
+          </button>
+        </Link>
+      </form>
+      <MyReports />
     </section>
   );
 };
