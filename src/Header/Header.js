8e9aceb0ec693bcc62e5135e00cf983dc368a230
kimmyfek <kimmyfek@gmail.com>
2023-04-16 10:34:35 -0600

    update header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -3,7 +3,10 @@ import React, { useState } from "react";
 const Header = () => {
   return (
     <div className="App-header">
-      <h1>ORCA</h1>
+      <div className="header-logo">
+        <img className="logo" src="orca.svg" alt="orca" />
+        <h2>O.R.C.A</h2>
+      </div>
     </div>
   );
 };
